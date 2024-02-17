b7a859e5edc5b150b08631de3fa0327e923c4c84
nazem9 <69907990+nazem9@users.noreply.github.com>
2024-02-17 20:48:22 +0300

    start

--- admin/AppAuth/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppAuthConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppAuth'
-    def ready(self) -> None:
-        from AppAuth import signals
